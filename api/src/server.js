bce6c28b01c47239750fadcbe9e6622ae0c6e633
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-23 11:55:09 -0300

    Add routes to server

--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,39 @@
 import express from 'express';
 import { Kafka } from 'kafkajs'
+import routes from './routes'
 
 const app = express();
 
+/**
+ * Faz conexão com o kafka
+ */
 const kafka = new Kafka({
     clientId: 'api', // identificador 
-    brokers: ['kafka:9092'] // instancia do kafka // ele usou docker (?) 31:11
+    brokers: ['localhost:9091'] // instancia do kafka // ele usou docker (?) 31:11
 })
 
+const producer = kafka.producer()
+
+/**
+ * Disponibiliza o producer para todas as rotas
+ */
+app.use((req, res, next) => {
+    req.producer = producer;
+
+    return next()
+})
+
+/**
+ * Cadastra as rotas da aplicação
+ */
+app.use(routes)
+
 async function run() {
-    await producer.connect();
-    
+    //await producer.connect();
+
     app.listen(3333)
 }
 
 run().catch(console.error);
 
-const producer = kafka.producer()
+
