7c8d4847ffb13ed2ab70c74f26cada4d3f635323
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-23 11:55:31 -0300

    Correct import and show producer on log

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,10 @@
-import  express  from express;
+import express from 'express';
 
 const routes =  new express.Router();
 
 routes.get('/certifications', async (req, res) => {
     // chamar micro servico
+    console.log(req.producer);
     return res.json({ ok: true })
 })
 
