03a514570454ecd077ceec5ab2df4af91860e052
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-24 09:57:36 -0300

    producer send message

--- api/src/routes.js
+++ api/src/routes.js
@@ -3,8 +3,22 @@ import express from 'express';
 const routes =  new express.Router();
 
 routes.get('/certifications', async (req, res) => {
+    const message = {
+        user: { id: 1, name: 'Talita Oliveira' },
+        course:  'Kafka com NodeJS',
+        grade: 5 
+    }
+
+
     // chamar micro servico
-    console.log(req.producer);
+    await req.producer.send({
+        topic: 'certificates',
+        messages: [
+            { value: JSON.stringify(message) }
+        ]
+    })
+    .then(console.log)
+    .catch(e => console.error(`[example/producer] ${e.message}`, e))
     return res.json({ ok: true })
 })
 
